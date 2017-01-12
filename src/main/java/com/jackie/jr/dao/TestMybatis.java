b531d6e5c8af0508ad7a1ea523e2bc876d511fe6
luhaiming <luhaiming@zillionfortune.com>
2017-01-12 17:27:36 +0800

    add

---
+++ src/main/java/com/jackie/jr/dao/TestMybatis.java
@@ -0,0 +1,20 @@
+package com.jackie.jr.dao;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by luhaiming on 2017/1/12 0012.
+ */
+public class TestMybatis {
+
+    public static void main(String [] args) throws IOException {
+        String resource = "org/mybatis/example/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+}
