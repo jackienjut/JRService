c3b2884535b961080efaa83205434264088a98ce
luhaiming <luhaiming@zillionfortune.com>
2017-03-10 15:33:25 +0800

    add all file

--- src/main/java/com/jackie/jr/controller/DataController.java
+++ src/main/java/com/jackie/jr/controller/DataController.java
@@ -1,7 +1,10 @@
 package com.jackie.jr.controller;
 
+import com.jackie.jr.utils.HttpUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
@@ -26,5 +29,14 @@ public class DataController {
     public String getSystemDate(HttpServletResponse response) throws IOException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyymmdd:hhmm");
         return sdf.format(new Date());
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/getstockdata/{stock_id}")
+    public String getStockDataById(@RequestParam(value = "stock_id") String stock_id) {
+        String url = "http://table.finance.yahoo.com/table.csv?s=%s.sz";
+        url = url.replace("%s", stock_id);
+        String res = HttpUtils.get(url);
+        return res;
     }
 }
